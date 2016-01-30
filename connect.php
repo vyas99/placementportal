e672ac1d189ef7bd58002500bd98a3fa4378890a
K L Bhanu <klbm9999@gmail.com>
2016-01-30 23:37:03 +0530

    Deleting conect.php to avoid security breaches

--- connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-	
-	$con=mysqli_connect("localhost","root","","placementportal");
-	if (mysqli_connect_errno($con))
-	{
-		echo "Failed to connect to MySQL: " . mysqli_connect_error();
-	}
-?>
