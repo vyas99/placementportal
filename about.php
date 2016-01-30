eec8f4db6110164d98bf0dd30f199680bfb37763
Shubham <ssshubham40@gmail.com>
2016-01-30 20:05:28 +0530

    added contact and about us page

---
+++ about.php
@@ -0,0 +1,43 @@
+<?php include 'h1.php'; ?>
+<?php include 'tab_style.php'; ?>
+
+<div class="row">
+
+  <div class="col-md-6 col-md-offset-3 text-center">
+
+    <h3 style="color:#015b64"><b>Placement Portal</b></h3>
+
+  </div>
+
+</div>
+
+<div class="row">
+
+	<div class="col-md-6 col-md-offset-3">
+	
+		<nav class="nav nav-tabs nav-justified">
+
+			<li><a href="javascript:history.go(-1)">Back</a></li>
+			<li><a href="home.php">Home</a></li>
+
+		</nav>
+
+	</div>
+
+</div>
+<h3 class="ttle text-center" style="font-size : 25px;">About IIITD&M</h3>
+
+<div class="row">
+
+	<div class="col-md-10 col-md-offset-1">
+
+		<p style="text-align:justify;">Globalization has created uniformity in customer expectations world over. With opening up of Indian economy, our manufacturing sector has to compete globally even for the domestic market. This would require strong products with leading technology / quality and compelling cost advantage. Suitably trained manpower with skill sets appropriate for design, development and prototyping using modern IT tools is critical to achieve this goal. This Institute is envisioned as an academic institution of excellence that would facilitate and promote the competitive advantage of Indian products in global markets. This Institute will serve as an inter-disciplinary institution for education and research in the area of Product Lifecycle Management (PLM) encompassing design and manufacturing using state of the art concepts, tools, processes and practices of the industry world over. IIT Madras has been entrusted with the responsibility of setting up this Institute. Classes will be conducted in the IIT Madras campus till Institute's facilities are created. Strong industry interface and close cooperation with other academic and research institutions will be the key features of this Institute. This Institute will give emphasis on synthesis, creativity, hands-on experience, innovation, communication and entrepreneurship. These qualities along with basic knowledge of design and manufacturing technologies will form the ethos of education at this Institute. Click <a href="http://www.iiitdm.ac.in/">here</a> to visit the institutes official website.</p>
+		</br></br>
+
+	</div>
+
+</div>
+
+
+<?php include 'footer.php'; ?>
+
---
+++ contact.php
@@ -0,0 +1,46 @@
+<?php include 'h1.php'; ?>
+<?php include 'tab_style.php'; ?>
+
+<div class="row">
+
+  <div class="col-md-6 col-md-offset-3 text-center">
+
+    <h3 style="color:#015b64"><b>Placement Portal</b></h3>
+
+  </div>
+
+</div>
+
+<div class="row">
+
+	<div class="col-md-6 col-md-offset-3">
+	
+		<nav class="nav nav-tabs nav-justified">
+
+			<li><a href="javascript:history.go(-1)">Back</a></li>
+			<li><a href="home.php">Home</a></li>
+
+		</nav>
+
+	</div>
+
+</div>
+
+
+<h3 class="ttle text-center" style="font-size : 25px;">Contact us:</h3>
+<div class="text-center">
+	Indian Institute of Information Technology,</br>
+	Design & Manufacturing (IIITD&M) Kancheepuram</br>
+	Melakottaiyur Village</br>
+	Off Vandalur-Kelambakkam Road</br>
+	Nellikuppam Road</br>
+	Chennai - 600 127</br>
+	Tamil Nadu</br>
+	India</br>
+	TEL +91-44-2747 6300</br>
+	EMAIL</br>
+	office@iiitdm.ac.in</br>
+</div>
+<p class="text-center">Click <a href="http://www.iiitdm.ac.in/">here</a> to visit the institutes official website.</p>
+<?php include 'footer.php'; ?>
+
