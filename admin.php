96249af43f8b552139fc3a3ad7ba89fa7e79f0dc
Shubham <ssshubham40@gmail.com>
2016-02-01 23:02:21 +0530

    admin page added

---
+++ admin.php
@@ -0,0 +1,26 @@
+<?php include 'h1.php'; 
+error_reporting(0);
+?>
+<?php include 'tab_style.php'; 
+?>
+<<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<div class="row-fluid">
+	<div class="com-md-6" style="background-color:red;">
+	
+
+
+	</div>
+	<div class="col-md-6">
+		
+	</div>
+</div>
+</body>
+</html>
+
+
+<?php include 'footer.php'; ?>
